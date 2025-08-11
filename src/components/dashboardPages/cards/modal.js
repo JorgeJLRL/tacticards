0976fd49682ba2d6f03d72a67305e2d364b70829
phalcobtw <laxz_16@hotmail.com>
2025-08-11 16:50:10 -0700

    nuevo diseño sin terminar para modal agregar card

--- src/components/dashboardPages/cards/modal.js
+++ src/components/dashboardPages/cards/modal.js
@@ -21,25 +21,29 @@ const style = {
 };
 
 const inputFields = [
-  { label: "Nombre de la tarjeta", propertyName: "nombreTarjeta" },
+  { label: "Nombre", propertyName: "nombreTarjeta" },
   { label: "Puesto", propertyName: "puesto" },
   { label: "Empresa", propertyName: "empresa" },
   { label: "Teléfono fijo", propertyName: "telefonoFijo" },
-  { label: "Teléfono móvil", propertyName: "telefonoMovil" },
+  { label: "Teléfono celular", propertyName: "telefonoMovil" },
   { label: "Dirección", propertyName: "direccion" },
-  { label: "Dirección de correo", propertyName: "direccionCorreo" },
+  { label: "Correo electrónico", propertyName: "direccionCorreo" },
   { label: "Sitio web", propertyName: "sitioWeb" },
+];
+
+const socialFields = [
   { label: "Facebook", propertyName: "facebook" },
   { label: "Instagram", propertyName: "instagram" },
   { label: "LinkedIn", propertyName: "linkedIn" },
   { label: "Ubicación", propertyName: "ubicacion" },
   { label: "WhatsApp Business", propertyName: "whatsappBusiness" },
+  { label: "Youtube", propertyName: "youtube" },
 ];
 
-const emptyFormValues = inputFields.reduce((acc, field) => ({ ...acc, [field.propertyName]: "" }), {
-  fotoPerfil: "",
-  fotoPortada: "",
-});
+const emptyFormValues = [...inputFields, ...socialFields].reduce(
+  (acc, field) => ({ ...acc, [field.propertyName]: "" }),
+  { fotoPerfil: "", fotoPortada: "" }
+);
 
 export default function CardsModal({ openModal, closeModal, addCard }) {
   const [open, setOpen] = React.useState(false);
@@ -156,22 +160,73 @@ export default function CardsModal({ openModal, closeModal, addCard }) {
                 />
               </Grid>
             </Grid>
-            <Grid container spacing={2} sx={{ paddingBottom: "0.5rem" }}>
-              {inputFields.map((field) => (
-                <Grid item xs={6} key={field.propertyName}>
-                  <TextField
-                    fullWidth
-                    margin="normal"
-                    label={field.label}
-                    variant="standard"
+            <div className="flex self-center w-full text-center justify-center mt-6 mb-6">
+              <p className="text-black font-semibold text-lg">Datos Visibles</p>
+            </div>
+            {inputFields.map((field) => (
+              <Grid item xs={12} key={field.propertyName}>
+                {/* <TextField
+                  fullWidth={true}
+                  xs={12}
+                  sx={{ backgroundColor: "#f5f5f5", borderRadius: "1rem" }}
+                  margin="normal"
+                  InputProps
+                  label={field.label}
+                  variant="standard"
+                  name={field.propertyName}
+                  onChange={(e) => setFormValues({ ...formValues, [e.target.name]: e.target.value })}
+                  value={formValues[field.propertyName]}
+                /> */}
+                <input
+                  type="text"
+                  name={field.propertyName}
+                  onChange={(e) => setFormValues({ ...formValues, [e.target.name]: e.target.value })}
+                  value={formValues[field.propertyName]}
+                  placeholder={field.label}
+                  style={{
+                    width: "100%",
+                    padding: "8px",
+                    marginBottom: "8px",
+                    borderRadius: "1rem",
+                    backgroundColor: "#f5f5f5",
+                    color: "black",
+                  }}
+                ></input>
+              </Grid>
+            ))}
+            <div className="flex self-center w-full text-center justify-center mt-6 mb-6">
+              <p className="text-black font-semibold text-lg">Hipervínculos</p>
+            </div>
+            <Grid container spacing={2} mb={2} sx={{ alignItems: "center", width: "100%", justifyContent: "center" }}>
+              {socialFields.map((field, idx) => (
+                <Grid
+                  item
+                  xs={socialFields.length % 2 !== 0 && idx === socialFields.length - 1 ? 12 : 6}
+                  key={field.propertyName}
+                  style={{ display: "flex", alignItems: "center", gap: "10px" }}
+                >
+                  <img src={`/images/${field.propertyName}.png`} style={{ width: "30px", height: "30px" }}></img>
+                  <input
+                    type="text"
                     name={field.propertyName}
                     onChange={(e) => setFormValues({ ...formValues, [e.target.name]: e.target.value })}
                     value={formValues[field.propertyName]}
+                    placeholder={field.label}
+                    style={{
+                      width: "100%",
+                      flex: 1,
+                      padding: "8px",
+                      marginBottom: "8px",
+                      borderRadius: "1rem",
+                      backgroundColor: "#f5f5f5",
+                      color: "black",
+                    }}
                   />
                 </Grid>
               ))}
-              <hr style={{ margin: "0 auto", backgroundColor: "gray", height: "1.5px", width: "100%" }}></hr>
             </Grid>
+
+            <hr style={{ margin: "0 auto", backgroundColor: "gray", height: "1.5px", width: "100%" }}></hr>
             <Grid container spacing={2} sx={{ alignItems: "end", paddingTop: "0.5rem" }}>
               {extraSocialFields.map((field, i) => (
                 <React.Fragment key={field.id}>
@@ -210,10 +265,30 @@ export default function CardsModal({ openModal, closeModal, addCard }) {
                 </React.Fragment>
               ))}
             </Grid>
+            <div className="flex self-center text-center justify-center mt-4">
+              <Button
+                onClick={handleAddSocialField}
+                variant="outlined"
+                sx={{
+                  borderRadius: "50%",
+                  height: "30px",
+                  width: "30px",
+                  minWidth: "30px",
+                  fontSize: "24px",
+                  textTransform: "none",
+                  color: "green",
+                  borderColor: "green",
+                  padding: 0,
+                  "&:hover": {
+                    backgroundColor: "rgba(0, 128, 0, 0.1)",
+                    borderColor: "green",
+                  },
+                }}
+              >
+                +
+              </Button>
+            </div>
 
-            <Button onClick={handleAddSocialField} variant="outlined" sx={{ mt: 2 }}>
-              Agregar red social
-            </Button>
             <Box mt={3} display="flex" justifyContent="flex-end">
               <Button variant="contained" onClick={sendForm} disabled={isLoading}>
                 {isLoading ? "Enviando..." : "Guardar"}
