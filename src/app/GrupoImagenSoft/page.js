3af1d1ef4fbb5a081b722ca07b8da84ae7009660
phalcobtw <laxz_16@hotmail.com>
2025-08-27 12:57:59 -0700

    trackit removido

--- src/app/GrupoImagenSoft/page.js
+++ src/app/GrupoImagenSoft/page.js
@@ -3,39 +3,18 @@ export default function GrupoImagenSoft() {
   return (
     <div className={styles.flexBoxGIS}>
       <div className={styles.ImageBoxFlex}>
-        <div className={styles.imageBox3}>
-          <img
-            className={styles.imgResponsive1}
-            src="/images/trackitagro-logo.png"
-          ></img>
-          <button className={styles.btnVerInfo}>
-            <a href="https://www.tacticards.com.mx/GrupoImagenSoft/Ubisat/">
-              Ver Información
-            </a>
-          </button>
-        </div>
         <div className={styles.imageBox2}>
-          <img
-            className={styles.imgResponsive}
-            src="/images/Ubisat-logo.png"
-          ></img>
+          <img className={styles.imgResponsive} src="/images/Ubisat-logo.png"></img>
           <button className={styles.btnVerInfo}>
-            <a href="https://www.tacticards.com.mx/GrupoImagenSoft/Ubisat/">
-              Ver Información
-            </a>
+            <a href="https://www.tacticards.com.mx/GrupoImagenSoft/Ubisat/">Ver Información</a>
           </button>
         </div>
 
         <div className={styles.imageBox1}>
-          <img
-            className={styles.imgResponsive}
-            src="/images/iGAS-logo.png"
-          ></img>
+          <img className={styles.imgResponsive} src="/images/iGAS-logo.png"></img>
           <div className={styles.btninfoBox}>
             <button className={styles.btnVerInfo}>
-              <a href="https://www.tacticards.com.mx/GrupoImagenSoft/iGAS/">
-                Ver Información
-              </a>
+              <a href="https://www.tacticards.com.mx/GrupoImagenSoft/iGAS/">Ver Información</a>
             </button>
           </div>
         </div>
